806ed270d07a1f39b98ff6ce57f5e920e902ecea
Leah Petersen <leahnpetersen@gmail.com>
2018-04-27 16:13:48 -0400

    gogo maas works now

--- charts/cluster-controller/controller-start.sh
+++ charts/cluster-controller/controller-start.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # echo a secret bro
 echo "$SECRET_USERNAME"
+
+# TODO: establish how controller will leverage gogo
+
+
+
+# start controller
+/bin/controller/main -kubeconfig=
